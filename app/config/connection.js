c55603dfb159d7c20fe7182d88d79ace28d86f77
Mike Gomez <mikegomez21@hotmail.com>
2018-06-10 12:29:30 -0500

    update

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,8 +4,19 @@
 
 // Dependencies
 var Sequelize = require("sequelize");
-
+var connection;
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+  })
+/*
 var sequelize = new Sequelize("chirpy", "newuser1", "m1234567", {
   host: "localhost",
   port: 3306,
@@ -14,8 +25,9 @@ var sequelize = new Sequelize("chirpy", "newuser1", "m1234567", {
     max: 5,
     min: 0,
     idle: 10000
-  }
+  }*/
 });
 
+connection.connect();
 // Exports the connection for other files to use
 module.exports = sequelize;
